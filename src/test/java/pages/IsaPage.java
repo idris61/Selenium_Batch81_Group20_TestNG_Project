7db59226d8d0ce586122b253afa2afb8b90be791
isa argundogan <ode121@gmail.com>
2022-10-24 20:01:20 +0300

    isa

--- src/test/java/pages/IsaPage.java
+++ src/test/java/pages/IsaPage.java
@@ -1,4 +1,22 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class IsaPage {
+
+    public IsaPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+@FindBy(xpath="//span[text()='Sign In']")
+public
+WebElement signIn;
+    @FindBy(xpath="//label[@for='username']")
+   public
+    WebElement username;
+    @FindBy(xpath = "//input[@id='password']")
+    public
+    WebElement password;
 }
--- src/test/java/tests/isa/US001_TC01.java
+++ src/test/java/tests/isa/US001_TC01.java
@@ -1,4 +1,20 @@
 package tests.isa;
 
+import org.testng.annotations.Test;
+import pages.IsaPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US001_TC01 {
+
+   IsaPage isaPage = new IsaPage();
+
+    @Test
+    public void testTc_01() {
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+        isaPage.signIn.click();
+        isaPage.username.sendKeys(ConfigReader.getProperty("vendorEmail"));
+        isaPage.password.sendKeys(ConfigReader.getProperty("vendorPassword"));
+    }
 }
+
