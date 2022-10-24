83aa169e3f4c74d26f5e102a3bd627344d119679
Naif aslan <naifasln3121@gmail.com>
2022-10-24 20:01:33 +0300

    naif

--- src/test/java/pages/NaifPage.java
+++ src/test/java/pages/NaifPage.java
@@ -1,4 +1,40 @@
 package pages;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class NaifPage {
+    public NaifPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement sign;
+
+    @FindBy(xpath = "(//input[@id='username'])[1]")
+    public WebElement username;
+
+    @FindBy(linkText = "My Orders")
+    public WebElement myOrders;
+
+    @FindBy(xpath = "//*[@Class='woocommerce-Button button btn btn-link btn-underline btn-icon-right']")
+    public WebElement Browseproducts;
+
+    @FindBy(xpath = "(//*[@Class='w-icon-cart'])[1]")
+    public WebElement cart;
+
+    @FindBy(xpath = "//*[@Class='button checkout wc-forward']")
+    public WebElement checkout;
+
+    @FindBy(xpath = "//*[@id='billing_first_name']")
+    public WebElement firstname;
+
+
+
 }
--- src/test/java/tests/naif/US003_TC01.java
+++ src/test/java/tests/naif/US003_TC01.java
@@ -1,4 +1,27 @@
 package tests.naif;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.NaifPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US003_TC01 {
+    NaifPage naifPage = new NaifPage();
+
+    @Test
+    public void testTC01() {
+
+
+
+    }
 }
