18777537edffbff6c0ac664608ca075ad8066946
Cengizhan Ketenci <cengizhanketenci79@gmail.com>
2022-10-25 19:05:31 +0300

    han

--- src/test/java/tests/cengizhan/US013_TC06.java
+++ src/test/java/tests/cengizhan/US013_TC06.java
@@ -1,4 +1,63 @@
 package tests.cengizhan;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CengizhanPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.time.Duration;
+
 public class US013_TC06 {
+
+    CengizhanPage page = new CengizhanPage();
+    JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+    Actions actions = new Actions(Driver.getDriver());
+    ExpectedConditions ExceptedCondition;
+
+
+    @Test
+    public void Test_US013_TC06() throws Exception {
+        // Belirtilen URL` ye gidilir
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+
+        //"sign in" tiklanir
+        page.signInWE.click();
+
+        //Gecerli veriler ile "sign in" yapilir
+        page.userNameWE.sendKeys(ConfigReader.getProperty("alloverValidEmail"));
+        page.passwordWE.sendKeys(ConfigReader.getProperty("alloverValidPassword"));
+        page.signInButtonWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.myAccountWE));
+
+        //"My Account" linki tiklanir (sayfanin altinda)
+        jse.executeScript("arguments[0].click();", page.myAccountWE);
+
+        // aynı işlemi actions class'ı kullanarak yapmak istersek.
+        // actions.sendKeys(Keys.PAGE_DOWN).perform();
+        // page.myAccountWE.click();
+
+        wait.until(ExceptedCondition.visibilityOf(page.storeManagerWE));
+
+        //"My Account" sayfasinda "Store Manager" linki tiklanir
+        page.storeManagerWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.couponsWE));
+
+        //"Store Manager" sayfasinda "Coupons" linki tiklanir
+        page.couponsWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.addNewWE));
+
+        //"Coupons" sayfasinda "Add New" butonu tiklanir
+        page.addNewWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.freeShippingWE));
+
+        //"Add Coupon" sayfasında "Allow free shipping" kutucugunun Enabled oldugu dogrulanır
+        Assert.assertTrue(page.freeShippingWE.isEnabled());
+        Driver.closeDriver();
+    }
 }
--- src/test/java/tests/cengizhan/US015_TC02.java
+++ src/test/java/tests/cengizhan/US015_TC02.java
@@ -1,4 +1,67 @@
 package tests.cengizhan;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CengizhanPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.time.Duration;
+
 public class US015_TC02 {
+
+    CengizhanPage page = new CengizhanPage();
+    JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+    Actions actions = new Actions(Driver.getDriver());
+    ExpectedConditions ExceptedCondition;
+
+
+    @Test
+    public void Test_US015_TC02() throws Exception {
+        // Belirtilen URL` ye gidilir
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+
+        //"sign in" tiklanir
+        page.signInWE.click();
+
+        //Gecerli veriler ile "sign in" yapilir
+        page.userNameWE.sendKeys(ConfigReader.getProperty("alloverValidEmail"));
+        page.passwordWE.sendKeys(ConfigReader.getProperty("alloverValidPassword"));
+        page.signInButtonWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.myAccountWE));
+
+        //"My Account" linki tiklanir (sayfanin altinda)
+        jse.executeScript("arguments[0].click();", page.myAccountWE);
+
+        // aynı işlemi actions class'ı kullanarak yapmak istersek.
+        // actions.sendKeys(Keys.PAGE_DOWN).perform();
+        // page.myAccountWE.click();
+
+        wait.until(ExceptedCondition.visibilityOf(page.storeManagerWE));
+
+        //"My Account" sayfasinda "Store Manager" linki tiklanir
+        page.storeManagerWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.couponsWE));
+
+        //"Store Manager" sayfasinda "Coupons" linki tiklanir
+        page.couponsWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.addNewWE));
+
+        //"Coupons" sayfasinda "Add New" butonu tiklanir
+        page.addNewWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.limitButtonWE));
+
+        //Acilan sayfada "Limit" butonu tiklanmalı
+        page.limitButtonWE.click();
+        wait.until(ExceptedCondition.visibilityOf(page.limitUsageToXItemsWE));
+
+        //Acilan sayfada "Limit usage to X items" kutusunun enabled oldugu dogrulanmalı
+        Assert.assertTrue(page.limitUsageToXItemsWE.isEnabled());
+        Driver.closeDriver();
+    }
 }
