512d48e0f14828f52de6c26d44323735ab018020
Naif aslan <naifasln3121@gmail.com>
2022-10-25 17:50:04 +0300

    naif

--- src/test/java/tests/naif/US003_TC04.java
+++ src/test/java/tests/naif/US003_TC04.java
@@ -1,4 +1,57 @@
 package tests.naif;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import pages.NaifPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.time.Duration;
+
 public class US003_TC04 {
+    @Test
+    public void testTC04() throws InterruptedException {
+        Actions actions = new Actions(Driver.getDriver());
+        NaifPage naifPage = new NaifPage();
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+
+        //Belirt,len Url'gidilir
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+        //"Sign" butonuna tıklanır
+        naifPage.sign.click();
+        //"Username or email adress" bölümüne vendor email girilir
+        naifPage.username.sendKeys(ConfigReader.getProperty("vendorEmail"));
+        //"Password" bölümüne verdor password girilir
+        actions.sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("vendorPassword")).sendKeys(Keys.ENTER).perform();
+        //"Sign" butonuna tıklanır
+        naifPage.sign2.click();
+        //"My orders" butonuna tıklanır
+        jse.executeScript("arguments[0].click();", naifPage.myOrders);
+        //"Go Shop" butonuna tıklanır
+        jse.executeScript("arguments[0].click();", naifPage.goshop);
+
+        //5 adet ürün secilir
+        jse.executeScript("arguments[0].click();", naifPage.basket);
+        jse.executeScript("arguments[0].click();", naifPage.triko);
+        jse.executeScript("arguments[0].click();", naifPage.canta);
+        jse.executeScript("arguments[0].click();", naifPage.crusher);
+        jse.executeScript("arguments[0].click();", naifPage.tarhana);
+        //"Cart" butonuna tıklanır
+        naifPage.cart.click();
+        //"Checkout"butonuna tıklanır
+        naifPage.checkout.click();
+        naifPage.firstname.sendKeys(ConfigReader.getProperty("Firstname"));
+        actions.sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("Lastname")).sendKeys().perform();
+
+
+
+
+    }
 }
+
+
+
