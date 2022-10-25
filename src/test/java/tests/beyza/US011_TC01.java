4208693b46908b0d4c253c2c5cf76597854c5638
bilal <bilal@classlist.com>
2022-10-25 19:51:24 +0300

    beyza

--- src/test/java/tests/beyza/US011_TC01.java
+++ src/test/java/tests/beyza/US011_TC01.java
@@ -1,4 +1,83 @@
 package tests.beyza;
 
-public class US011_TC01 {
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import pages.BeyzaPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class US011_TC01 extends TestBaseRapor {
+    BeyzaPage beyzaPage = new BeyzaPage();
+    Actions actions = new Actions(Driver.getDriver());
+    @Test
+    public void testTC001() {
+        extentTest = extentReports.createTest("alloverUrl", "Web automation Raporlama");
+
+
+
+
+        // Belirtilen URL` ye gidilir
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+        extentTest.info("belirtilen URL'e gidildi");
+
+
+        // Singin butonuna tıkalnır
+        beyzaPage.signIn.click();
+        extentTest.info("Singin buttonu na tıklandi");
+
+        // Gecerli bir username girilir
+        beyzaPage.username.sendKeys(ConfigReader.getProperty("alloverValidEmail"));
+        extentTest.info("Gecerli username girildi");
+
+        // Gecerli bir password girilir
+        actions.sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("alloverValidPassword"));
+        extentTest.info("gecerli password girildi");
+
+        // Singin butonuna basilir.
+        actions.sendKeys(Keys.ENTER).perform();
+        extentTest.info("singin buttonu na tıklandi");
+
+        // My Accaunt butonuna tıklanir
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+        jse.executeScript("arguments[0].scrollIntoView(true);", beyzaPage.myAccountSignOutLink);
+        jse.executeScript("arguments[0].click();", beyzaPage.myAccountSignOutLink);
+
+        extentTest.info("My Accaunt a tıklandı");
+
+
+        // Stor Manager butonuna tıklanir
+        beyzaPage.storManager.click();
+        extentTest.info("Stor Manger'a tiklandi");
+
+
+        // Products butonuna tıklanır
+
+        jse.executeScript("arguments[0].scrollIntoView(true);", beyzaPage.products);
+        jse.executeScript("arguments[0].click();", beyzaPage.products);
+
+
+        // Add New butonuna tıklanır
+        beyzaPage.addNew.click();
+
+        // Toptan urun gosterme ayarlari' butonuna tiklar
+        jse.executeScript("arguments[0].scrollIntoView(true);", beyzaPage.toptanUrunlerGostermeAyarlari);
+        jse.executeScript("arguments[0].click();", beyzaPage.toptanUrunlerGostermeAyarlari);
+
+
+        // Piece Type' butonuna tiklar'
+        // piece' si secer
+
+        Select select = new Select(beyzaPage.pieceDdm);
+        select.selectByIndex(1);
+        extentTest.pass("piece 'i secti" );
+
+
+
+
+    }
 }
--- src/test/java/tests/beyza/US021_TC01.java
+++ src/test/java/tests/beyza/US021_TC01.java
@@ -1,4 +1,66 @@
 package tests.beyza;
 
-public class US021_TC01 {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.BeyzaPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class US021_TC01 extends TestBaseRapor {
+
+    BeyzaPage beyzaPage = new BeyzaPage();
+    Actions actions = new Actions(Driver.getDriver());
+
+    @Test
+    public void testTC002() {
+        extentTest = extentReports.createTest("alloverUrl", "Web automation Raporlama");
+
+
+        // Belirtilen URL` ye gidilir
+        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
+        extentTest.info("belirtilen URL'e gidildi");
+
+        // Singin butonuna tıkalnır
+        beyzaPage.signIn.click();
+        extentTest.info("Singin buttonu na tıklandi");
+
+        // Gecerli bir username girilir
+        beyzaPage.username.sendKeys(ConfigReader.getProperty("alloverValidEmail"));
+        extentTest.info("Gecerli username girildi");
+
+        // Gecerli bir password girilir
+        actions.sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("alloverValidPassword"));
+        extentTest.info("gecerli password girildi");
+
+        // Singin butonuna basilir.
+        actions.sendKeys(Keys.ENTER).perform();
+
+        // My Accaunt butonuna tıklanir
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+        jse.executeScript("arguments[0].scrollIntoView(true);", beyzaPage.myAccountSignOutLink);
+        jse.executeScript("arguments[0].click();", beyzaPage.myAccountSignOutLink);
+
+        // Stor Manager butonuna tıklanir
+        beyzaPage.storManager.click();
+
+        // Reports butonuna tıklanır
+        jse.executeScript("arguments[0].scrollIntoView(true);", beyzaPage.reports);
+        jse.executeScript("arguments[0].click();", beyzaPage.reports);
+
+        // Add New butonuna tıklanır
+        beyzaPage.addNew.click();
+
+        //  Year gorunurlugu dogrulanir
+
+
+        //  Last Month gorunurlugu dogrulanir
+
+        //  This Month gorunurlugu dogrulanir
+
+        //   Last 7 Days gorunurlugu dogrulanir
+
+    }
 }
