e4a0ee469c2710fc8365f12b9e3f821977a3b3ae
isa argundogan <ode121@gmail.com>
2022-10-26 19:58:04 +0300

    isa

--- src/test/java/pages/IsaPage.java
+++ src/test/java/pages/IsaPage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class IsaPage {
+
+    public IsaPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath="//span[text()='Sign In']")
+    public WebElement signIn;
+    @FindBy(xpath="(//input[@id='username'])[1]")
+    public WebElement username;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement password;
 }
